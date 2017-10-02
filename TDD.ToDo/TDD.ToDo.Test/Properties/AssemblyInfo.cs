272d643cf37e29cffee44f182e2895591912f3e6
Arif Mosharraf <arif.mosharraf@hssbc.ca>
2017-10-02 15:38:25 -0700

    init

---
+++ TDD.ToDo/TDD.ToDo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDD.ToDo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Health Shared Services BC")]
+[assembly: AssemblyProduct("TDD.ToDo.Test")]
+[assembly: AssemblyCopyright("Copyright © Health Shared Services BC 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c28bc923-dca5-43fe-9853-0620e28a6583")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
